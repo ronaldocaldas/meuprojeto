c389cf43f893abd285603b07a5af82fd2aa431d5
Ronaldo Caldas da Silva <ronaldocaldas@gmail.com>
2018-05-27 12:37:57 -0300

    Exportando a API da diretiva para o template

--- src/app/campo-colorido.directive.ts
+++ src/app/campo-colorido.directive.ts
@@ -1,18 +1,19 @@
 import { Directive, HostListener, HostBinding, Input } from '@angular/core';
 
 @Directive({
-  selector: '[appCampoColorido]'
+  selector: '[appCampoColorido]',
+  exportAs: 'campoColorido'
 })
 export class CampoColoridoDirective {
   @Input('appCampoColorido') cor = 'gray';
   @HostBinding('style.backgroundColor') corDeFundo: String;
-  @HostListener('focus') aoGanharFoco() {
+  @HostListener('focus') colorir() {
     // this.renderer.setStyle(this.elementRef.nativeElement,
     //   'background-color', 'yellow');
     this.corDeFundo = this.cor;
   }
 
-  @HostListener('blur') aoPerderFoco() {
+  @HostListener('blur') descolorir() {
     // this.renderer.setStyle(this.elementRef.nativeElement,
     //   'background-color', 'transparent');
     this.corDeFundo = 'transparent';
