9a23aac946805c1c704d98feaece20f0d828b1cc
Ronaldo Caldas da Silva <ronaldocaldas@gmail.com>
2018-05-27 12:06:21 -0300

    Criando diretiva customizadas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,12 @@ import { HelloComponent } from './hello/hello.component';
 import { BemVindoComponent } from './bem-vindo/bem-vindo.component';
 import { FuncionarioCardComponent } from './funcionario-card/funcionario-card.component';
 import { FuncionarioFormComponent } from './funcionario-form/funcionario-form.component';
+import { CampoColoridoDirective } from './campo-colorido.directive';
 
 @NgModule({
   declarations: [
     AppComponent, 
-    HelloComponent, BemVindoComponent, FuncionarioCardComponent, FuncionarioFormComponent
+    HelloComponent, BemVindoComponent, FuncionarioCardComponent, FuncionarioFormComponent, CampoColoridoDirective
   ],
   imports: [
     BrowserModule,
